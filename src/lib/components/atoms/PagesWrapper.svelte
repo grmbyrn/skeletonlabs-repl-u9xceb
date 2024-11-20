701c4e7366f42660401d9b06d6fc9c771836ebe0
grmbyrn <git@stackblitz.com>
2024-11-20 15:29:03 +0100

    Initial commit

---
+++ src/lib/components/atoms/PagesWrapper.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	export let heading: string;
+</script>
+
+<div class="para">
+	{#if heading}
+		<h1>{heading}</h1>
+	{/if}
+	<div class="layout">
+		<slot />
+	</div>
+</div>
+
+<style>
+
+	.para {
+		display: flex;
+		flex-direction: column;
+		max-width: 1100px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		padding-inline: 2.5rem;
+		font-size: 2rem;
+		padding-top: 6rem;
+	}
+
+	.layout {
+		display: flex;
+		flex-direction: column;
+		padding-top: 4rem;
+		padding-inline: 2.5rem;
+	}
+
+  @media (min-width: 1200px) {
+    .layout {
+      flex-direction: row;
+      gap: 4rem;
+    }
+  }
+</style>
